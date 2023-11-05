9786f07d8f3d3f9d89d2e7342a65fb4eb26cd201
James Coward <james.sy.coward@gmail.com>
2023-11-05 13:17:28 +0000

    i don't like django

--- things/forms.py
+++ things/forms.py
@@ -1,12 +1,11 @@
 """Forms of the project."""
-
-# Create your forms here.
+from django.core.validators import RegexValidator
 from django import forms
 from .models import Thing
 
 class ThingForm(forms.ModelForm):
-    """Form for the Thing model."""
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
-        widgets = { 'descrption': forms.Textarea(), "quantity": forms.NumberInput()}
+        widgets = { 'description': forms.Textarea(), 'quantity': forms.NumberInput()}
+ 
--- things/views.py
+++ things/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from things.forms import ThingForm
+from .forms import ThingForm
 
 def home(request):
     form = ThingForm()
-    return render(request, 'things/home.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
