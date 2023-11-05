2c047a87aaf5a1ac78808363b2628ea76fdff305
James Coward <james.sy.coward@gmail.com>
2023-11-05 13:04:21 +0000

    from

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    """Form for the Thing model."""
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = { 'descrption': forms.Textarea(), "quantity": forms.NumberInput()}
+
+    
